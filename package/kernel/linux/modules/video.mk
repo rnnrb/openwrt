cf73fa4d0adc6c3c8bbd95b51aefb74684194c3f
Elliott Mitchell <ehem+openwrt@m5p.com>
2024-01-02 08:13:56 -0800

    package/kernel/video: replace wildcard with explicit name list
    Remove the use of shell wildcard in $(FILES) for kmod-video-dma.  This
    approach really shouldn't have been used, so clean this.
    
    Now use $(wildcard ) as imx/cortexa7 manages to omit
    videobuf2-dma-sg.ko.
    
    Signed-off-by: Elliott Mitchell <ehem+openwrt@m5p.com>

--- package/kernel/linux/modules/video.mk
+++ package/kernel/linux/modules/video.mk
@@ -1129,7 +1129,9 @@ define KernelPackage/video-dma
   KCONFIG:= \
 	CONFIG_VIDEOBUF2_DMA_CONTIG \
 	CONFIG_VIDEOBUF2_DMA_SG
-  FILES:= $(LINUX_DIR)/drivers/media/common/videobuf2/videobuf2-dma-*.ko
+  FILES:= \
+	$(LINUX_DIR)/drivers/media/common/videobuf2/videobuf2-dma-contig.ko \
+	$(wildcard $(LINUX_DIR)/drivers/media/common/videobuf2/videobuf2-dma-sg.ko)
   AUTOLOAD:=$(call AutoLoad,66,videobuf2-dma-contig videobuf2-dma-sg)
   $(call AddDepends/video)
 endef
